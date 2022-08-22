cfaa933979ac691d42843ab82bcba075210a7996
leon0113 <tahjib.leon399@gmail.com>
2022-08-23 04:38:50 +0600

    complete project

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// ! Pin generator section
 function getPin() {
     const pin = Math.round(Math.random() * 10000);
     const pinString = pin + '';
@@ -12,4 +13,39 @@ function getPin() {
 function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
+}
+//! User Pin section
+document.getElementById('keypad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-num');
+    if (isNaN(number)) {
+        // console.log(number);
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+})
+
+
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typed = document.getElementById('typed-num').value;
+
+    const pinPass = document.getElementById('pin-pass');
+    const pinFail = document.getElementById('pin-fail');
+    if (pin == typed) {
+
+        pinPass.style.display = 'block';
+        pinFail.style.display = 'none';
+    }
+    else {
+
+        pinFail.style.display = 'block';
+        pinPass.style.display = 'none';
+    }
 }
